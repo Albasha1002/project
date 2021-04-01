562918c78272f6fa89e7c79c49a9f2764251e81c
Albasha1002 <78254037+Albasha1002@users.noreply.github.com>
2021-04-01 09:16:26 +0530

    Create bashademo.py

---
+++ bashademo.py
@@ -0,0 +1,11 @@
+def search(list):
+   for i in range(5):
+      minlist=i
+       for j in range(i,6):
+         if list[j]<list[minlist]
+            minlis=j
+        list[i],list[minlist]=list[minlist],list[j]
+        
+list=[65,67,68,75,87,98]
+sort(list)
+print(list)
